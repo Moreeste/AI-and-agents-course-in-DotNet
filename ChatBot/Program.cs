be0e48da1ef0badca1111aef489303f542908d49
Esteban Rojas Huerta <esteban_moreeste@hotmail.com>
2026-07-22 00:19:21 -0600

    Pregunta a chat

--- ChatBot/Program.cs
+++ ChatBot/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using ChatBot;
+using OpenAI.Chat;
+
+Utilidades.CargarVariablesDeEntorno();
+
+var modelo = "gpt-5.4-nano";
+var key = Environment.GetEnvironmentVariable("OPENAI_API_KEY");
+
+var cliente = new ChatClient(modelo, key);
+
+var respuesta = await cliente.CompleteChatAsync("Hola, ¿cómo estás?");
+
+Console.WriteLine($"AI: {respuesta.Value.Content[0].Text}");
---
+++ ChatBot/Utilidades.cs
@@ -0,0 +1,17 @@
+﻿namespace ChatBot
+{
+    public class Utilidades
+    {
+        public static void CargarVariablesDeEntorno()
+        {
+            foreach (var linea in File.ReadAllLines(".env"))
+            {
+                var partes = linea.Split('=');
+                if (partes.Length == 2)
+                {
+                    Environment.SetEnvironmentVariable(partes[0], partes[1]);
+                }
+            }
+        }
+    }
+}
