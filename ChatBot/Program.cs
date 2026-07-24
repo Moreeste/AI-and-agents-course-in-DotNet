d0345c52479a1ad51f7e48539178b386fb67d65e
Esteban Rojas Huerta <esteban_moreeste@hotmail.com>
2026-07-24 00:11:17 -0600

    IChatClient

--- ChatBot/Program.cs
+++ ChatBot/Program.cs
@@ -1,6 +1,11 @@
 ﻿using ChatBot;
 using ChatBot.ChatBots;
+using Microsoft.Extensions.AI;
 
 Utilidades.CargarVariablesDeEntorno();
 
-await ChatbotOpenAI.Run();
+var modelo = "gpt-5.4-nano";
+var key = Environment.GetEnvironmentVariable("OPENAI_API_KEY");
+var cliente = new OpenAI.Chat.ChatClient(modelo, key).AsIChatClient();
+
+await ChatBotBase.Run(cliente);
