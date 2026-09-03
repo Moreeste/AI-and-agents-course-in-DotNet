8ccf3ac5b07331dc70df297859c6c9749ac9d8d4
Esteban Rojas Huerta <esteban_moreeste@hotmail.com>
2026-09-03 00:23:09 -0600

    chatbot real

--- BlazorIA/Program.cs
+++ BlazorIA/Program.cs
@@ -1,5 +1,6 @@
 using BlazorIA.Components;
 using BlazorIA.Servicios.ChatBots;
+using Microsoft.Extensions.AI;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +8,33 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddScoped<IChatBot, ChatBotFalso>();
+builder.Services.AddScoped<IChatBot, ChatBotReal>();
+
+builder.Services.AddSingleton<IChatClient>(sp =>
+{
+    var configuration = sp.GetRequiredService<IConfiguration>();
+    var proveedor = "openai";
+    var modelo = "gpt-5.4-nano";
+    var openAiKey = configuration.GetValue<string>("OpenAI_Key");
+
+    var cliente = proveedor switch
+    {
+        "openai" => new OpenAI.Chat.ChatClient(modelo ?? "gpt-5.4-nano", openAiKey).AsIChatClient(),
+        _ => throw new ArgumentException($"Proveedor desconocido: {proveedor}"),
+    };
+
+    return cliente.AsBuilder()
+    .ConfigureOptions(o =>
+    {
+        o.MaxOutputTokens = 2000;
+        o.Temperature = 0.7f;
+    })
+    .UseFunctionInvocation(null, c =>
+    {
+        c.IncludeDetailedErrors = true;
+    })
+    .Build(sp);
+});
 
 var app = builder.Build();
 
---
+++ BlazorIA/Servicios/ChatBots/ChatBotReal.cs
@@ -0,0 +1,98 @@
+﻿using BlazorIA.DTOs;
+using Microsoft.Extensions.AI;
+
+namespace BlazorIA.Servicios.ChatBots
+{
+    public class ChatBotReal : IChatBot
+    {
+        private readonly IChatClient _cliente;
+        private readonly List<ChatMessage> mensajes = [];
+
+        public List<MensajeChatUI> Conversacion { get; } = [];
+        public bool EstaProcesando { get; private set; }
+        public event Action? OnChange;
+
+        public ChatBotReal(IChatClient cliente)
+        {
+            _cliente = cliente;
+
+            var systemPromptGeneral = """
+            Eres un asistente que responde preguntas generales.
+            Debes responder en español.
+            Las respuestas deben ser en texto plano, no usar formatos como markdown.
+            Las respuestas deben ser concisas a menos que te indiquen lo contrario.
+
+            Si un tool falla, lee el mensaje de la excepción para ver si puedes arreglarlo haciendo algún ajuste. Comunícale al usuario cualquier ajuste que vayas a hacer.
+            """;
+
+            mensajes.Add(new ChatMessage(ChatRole.System, systemPromptGeneral));
+        }
+
+        public void CancelarRespuestaActual()
+        {
+
+        }
+
+        public async Task EnviarMensajeAsync(string textoUsuario, CancellationToken cancellationToken = default)
+        {
+            if (string.IsNullOrWhiteSpace(textoUsuario))
+            {
+                return;
+            }
+
+            if (EstaProcesando)
+            {
+                return;
+            }
+
+            EstaProcesando = true;
+
+            Conversacion.Add(new MensajeChatUI
+            {
+                Rol = RolMensaje.Usuario,
+                Texto = textoUsuario
+            });
+
+            mensajes.Add(new ChatMessage(ChatRole.User, textoUsuario));
+
+            Conversacion.Add(new MensajeChatUI
+            {
+                Rol = RolMensaje.IA,
+                Texto = string.Empty
+            });
+
+            NotificarCambio();
+            await ProcesarRespuesta(cancellationToken);
+            EstaProcesando = false;
+        }
+
+        private async Task ProcesarRespuesta(CancellationToken cancellationToken)
+        {
+            var updates = new List<ChatResponseUpdate>();
+
+            await foreach (var update in _cliente.GetStreamingResponseAsync(mensajes, cancellationToken: cancellationToken))
+            {
+                updates.Add(update);
+
+                foreach (var content in update.Contents)
+                {
+                    if (content is TextContent textContent)
+                    {
+                        Conversacion[^1].Texto += textContent.Text;
+                        NotificarCambio();
+                    }
+                }
+
+                var respuesta = updates.ToChatResponse();
+                mensajes.AddMessages(respuesta);
+            }
+        }
+
+        private void NotificarCambio() => OnChange?.Invoke();
+
+        public Task ResolverAprobacionAsync(bool aprobada, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
